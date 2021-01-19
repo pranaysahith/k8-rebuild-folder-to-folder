6dc81d599913b2390089ca9957c2a32d24273a85
Pranay Sahith Bejgum <pranaysahith7@gmail.com>
2021-01-19 16:09:28 +0530

    Add Github Actions workflow to build AMI and OVA (#1)
    * initial setup
    
    * update repo
    
    * add script to import ova
    
    * use Amazon Linux 2
    
    * apply dos2unix
    
    * delete
    
    * update s3 folder name
    
    * update scripts
    
    * add workflow trigger
    
    * fix syntax
    
    * Add step to import ova
    
    * attach and mount volume
    
    * update script to attach volume
    
    * build with custom Ubuntu image
    
    * pass ssh password
    
    * add gitignore
    
    * fix syntax
    
    * update base ami
    
    * update correct ami id
    
    * add quotes around variable value
    
    * add input to skip ova import
    
    * enable importing ova
    
    * remove other branch triggers
    
    * update tags
    
    * update auth step
    
    Co-authored-by: Pranay Bejgum <prbejgum@microsoft.com>

---
+++ packer/import-ova.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+OVA_NAME=${OVA_NAME:-$1}
+cat > packer/containers.json <<EOF
+[
+    {
+        "Description": "filedrop OVA",
+        "Format": "ova",
+        "UserBucket": {
+            "S3Bucket": "glasswall-sow-ova",
+            "S3Key": "vms/k8-rebuild-folder-to-folder/${OVA_NAME}"
+      }
+    }
+]
+EOF
+IMPORT_TASK=$(aws ec2 import-image --description "k8-rebuild-folder-to-folder" --disk-containers "file://packer/containers.json")
+IMPORT_ID=$(echo $IMPORT_TASK | jq -r .ImportTaskId)
+until [ "$RESPONSE" = "completed" ]
+do
+  RESPONSE=$(aws ec2 describe-import-image-tasks --import-task-ids $IMPORT_ID | jq -r '.ImportImageTasks[0].Status')
+  StatusMessage=$(aws ec2 describe-import-image-tasks --import-task-ids $IMPORT_ID | jq -r '.ImportImageTasks[0].StatusMessage')
+  if [[ "deleted" == "$RESPONSE" ]]; then
+    echo "Failed to import OVA"
+    echo "OVA Import status is $RESPONSE"
+    echo "Status message is $StatusMessage"
+    exit -1
+  elif [[ "active" == "$RESPONSE" ]]; then
+    echo "OVA Import status is $RESPONSE"
+    echo "Status message is $StatusMessage"
+    sleep 30
+  fi
+done
+echo "OVA Import status is $RESPONSE"
+AMI_ID=${echo $IMPORT_TASK }
+echo "Imported AMI ID is: ${AMI_ID}"
---
+++ packer/ova-check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+EXPORT_ID=$(cat out.json | jq -r .[].ExportTaskId)
+echo $EXPORT_ID
+# increase file size until 1KB
+until [ "$RESPONSE" = "completed" ]
+do
+  RESPONSE=$(aws ec2 describe-export-tasks --export-task-ids $EXPORT_ID | jq -r '.ExportTasks[].State')
+  if [ "$RESPONSE" != "exit" ]; then
+    echo "OVA export in progress..."
+    sleep 30
+  fi
+done
+
+echo "Upload Completed !!!"
+
+BUCKET=$(cat packer/ova-export.json | jq -r .S3Bucket)
+OBJECT="$(aws s3 ls $BUCKET/vms/k8-rebuild-folder-to-folder/ | sort | tail -n 1 | awk '{print $4}')"
+aws s3 mv s3://${BUCKET}/vms/k8-rebuild-folder-to-folder/${OBJECT} s3://${BUCKET}/vms/k8-rebuild-folder-to-folder/${OVA_NAME}
+
