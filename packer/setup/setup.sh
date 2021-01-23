b3906b3e667378a821680647ec6f13045d2c78f3
Pranay <pranaysahith7@gmail.com>
2021-01-23 02:16:50 +0000

    update docker-compose

--- packer/setup/setup.sh
+++ packer/setup/setup.sh
@@ -34,6 +34,7 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 # start applications
-mkdir -p /data/folder-to-folder/input && mkdir -p /data/folder-to-folder/output && mkdir -p /data/folder-to-folder/error && mkdir -p /data/folder-to-folder/log
+sudo mkdir -p /data/folder-to-folder/input && sudo mkdir -p /data/folder-to-folder/output && sudo mkdir -p /data/folder-to-folder/error && sudo mkdir -p /data/folder-to-folder/log
+sudo chown -R $USER:$USER /data/folder-to-folder
 sudo docker-compose up -d
 sleep 10s
